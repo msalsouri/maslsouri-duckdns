ab8beb8a7ea4f9518a4bbb88ca7ceccfb0d21e14
msalsouri <msalsouri@hotmail.com>
2024-07-09 03:10:45 +0100

    Add hoi-stripe-payments plugin structure and initial files

---
+++ dist/plugins/hoi-stripe-payments/hoi-stripe-payments.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Hoi Stripe Payments
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     hoi-stripe-payments
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Hoi_Stripe_Payments
+ */
+
+// Your code starts here.
