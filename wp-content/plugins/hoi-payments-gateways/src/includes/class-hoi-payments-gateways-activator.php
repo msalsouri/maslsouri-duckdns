17e7d48704719cb4eff32f90313d1da31ec0a8ee
msalsouri <msalsouri@hotmail.com>
2024-07-12 22:13:00 +0100

    Removed old plugin files and committed new plugin structure

---
+++ wp-content/plugins/hoi-payments-gateways/src/includes/class-hoi-payments-gateways-activator.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    Hoi_Payments_Gateways
+ * @subpackage Hoi_Payments_Gateways/includes
+ */
+
+/**
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Hoi_Payments_Gateways
+ * @subpackage Hoi_Payments_Gateways/includes
+ * @author     Your Name <email@example.com>
+ */
+class Hoi_Payments_Gateways_Activator {
+
+    /**
+     * Short Description. (use period)
+     *
+     * Long Description.
+     *
+     * @since    1.0.0
+     */
+    public static function activate() {
+        // Activation code here
+    }
+
+}
+
